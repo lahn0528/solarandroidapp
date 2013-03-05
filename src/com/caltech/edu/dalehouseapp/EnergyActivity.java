5a35b9f4cd9de0e4824eeff2d42f7f2ce2217e7e
cronostorm <cronostorm@gmail.com>
2013-03-04 19:20:54 -0800

    Have a http request working in the energy tab

--- src/com/caltech/edu/dalehouseapp/EnergyActivity.java
+++ src/com/caltech/edu/dalehouseapp/EnergyActivity.java
@@ -1,17 +1,27 @@
 package com.caltech.edu.dalehouseapp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class EnergyActivity extends Activity {
-
+	
+	 
     public void onCreate(Bundle  savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.energy_layout);
         
         TextView textview = new TextView(this);
-        textview.setText("This is Energy tab");
+        String responseString = "asd";
+		RequestTask r = new RequestTask(this);
+		r.execute("http://www.google.com");
+		//responseString = r.getstr();
+        textview.setText(responseString);
         setContentView(textview);
     }
 }
